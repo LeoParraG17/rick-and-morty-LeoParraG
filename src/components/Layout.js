fdfec4afb092eebf75c751b3fe42b2c0e278836d
LعÖ PaЯяa ĞalعaNo <leonardo.parra48260@ucaldas.edu.co>
2024-11-12 20:50:29 -0500

    actualizacion

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,7 @@ const Layout = () => {
                         
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active" aria-current="page" href="#">Informacion</a>
                             </li>
                         </ul>
                     </div>
